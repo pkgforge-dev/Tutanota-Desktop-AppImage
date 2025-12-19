9667e085c8de4837c72563744f0c8d1cedec4dbf
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-19 22:21:41 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(cat ~/version)
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
