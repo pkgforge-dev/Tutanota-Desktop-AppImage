684012496bf54d85c17cab767bed1e8a91c42b38
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-28 17:45:20 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,4 +8,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-PRE_BUILD_CMDS='sed "s/sha256sums=(\*/sha256sums=('SKIP'/g" ./PKGBUILD' make-aur-package tutanota-desktop
+PRE_BUILD_CMDS='sed -i "s/sha256sums=(\*/sha256sums=('SKIP'/g" ./PKGBUILD' make-aur-package tutanota-desktop
