da1b96671412d9c22e8c3f5dc5b24920a28e85ad
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-28 17:50:31 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,6 +6,6 @@ ARCH=$(uname -m)
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ! llvm-libs
 
 PRE_BUILD_CMDS='sed -i "s/sha256sums=(\*/sha256sums=('SKIP'/g" ./PKGBUILD' make-aur-package tutanota-desktop
