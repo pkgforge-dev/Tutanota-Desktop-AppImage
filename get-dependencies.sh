460ebe59615d2886679d80073b65157761444379
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-28 19:36:02 +0100

    Revert to previous building approach

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,10 +4,50 @@ set -eu
 
 ARCH=$(uname -m)
 
-pacman -Syu --noconfirm sqlcipher
+echo "Downloading and extracting Tutanota tar file..."
+echo "---------------------------------------------------------------"
+
+TAR_LINK=$(curl -s https://api.github.com/repos/tutao/tutanota/releases | 
+           grep -oP '"name": "\K[^"]*Desktop[^"]*' | 
+           head -n 1 | 
+           xargs -I {} curl -s https://api.github.com/repos/tutao/tutanota/releases | 
+           grep -oP '"browser_download_url": "\K[^"]*\.tar\.gz' |
+           head -n 1)
+
+VERSION=$(echo "$TAR_LINK" | grep -oP 'tutanota-desktop-release-\K[^/]+')
+
+if ! wget --retry-connrefused --tries=30 "$TAR_LINK" -O ./tuta.tar.gz 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+
+tar -xvf ./tuta.tar.gz
+rm -f ./tuta.tar.gz
+
+mkdir -p ./AppDir/bin
+cp -rv ./linux-unpacked/* ./AppDir/bin/
+cp -v ./linux-unpacked/resources/icons/icon/512.png ./AppDir/.DirIcon
+cp -v ./linux-unpacked/resources/icons/icon/512.png ./AppDir/tutanota-desktop.png
+
+cat << 'EOF' > ./AppDir/tutanota-desktop.desktop
+[Desktop Entry]
+Name=Tuta Mail
+Exec=tutanota-desktop
+Terminal=false
+Type=Application
+Icon=tutanota-desktop
+StartupWMClass=tutanota-desktop
+Comment=The desktop client for Tutanota, the secure e-mail service.
+MimeType=x-scheme-handler/mailto;
+Categories=Network;
+EOF
+
+echo "$VERSION" > ~/version
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm nspr nss
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ! llvm-libs
-
-PRE_BUILD_CMDS='sed -i "s/sha256sums=(\*/sha256sums=('SKIP'/g" ./PKGBUILD' make-aur-package tutanota-desktop
+get-debloated-pkgs --add-common --prefer-nano
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q tutanota-desktop | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/512x512/apps/tutanota-desktop.png
-export DESKTOP=/usr/share/applications/tutanota-desktop.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-mkdir -p ./AppDir/bin
-cp -rv /opt/tutanota-desktop/* ./AppDir/bin/
 quick-sharun ./AppDir/bin/* \
-             /usr/lib/libsecret* \
-             /usr/lib/libsqlcipher*
+             /usr/lib/libsecret*
 
 # This is hardcoded to look into /usr/bin/ldd and causes a crash
 # looks like we only need to patch this path away, it seems to work without it
